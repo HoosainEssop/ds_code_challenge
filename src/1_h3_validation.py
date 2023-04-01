a7bb00cd951c907c9439e4d08f0a620de342e9ab
HoosainEssop <mh.essop@gmail.com>
2023-04-01 17:49:06 +0200

    Added timeit and logging to challenge 1 solution

--- src/1_h3_validation.py
+++ src/1_h3_validation.py
@@ -5,29 +5,57 @@ from packages.boto_service import (S3_REGION,
                                    s3_select_query,
                                    s3_stream_to_dict)
 import geopandas as gpd
+from loguru import logger
+import timeit
+
 
 def validation_run():
-    # Download File and create dataframe
+    # init client
     s3_client = init_s3_client(S3_REGION)
-    obj_downloaded = download_s3_object(s3_client, S3_BUCKET, 'city-hex-polygons-8.geojson')
-    if obj_downloaded:
-        validate_df = gpd.read_file('./city-hex-polygons-8.geojson', driver="GeoJSON")
-        
 
     # Select stream data and create dataframe
     s3_select_query_result = s3_select_query(s3_client, S3_BUCKET, 'city-hex-polygons-8-10.geojson', 8)
+
+    # Parse Stream data to list of dicts
     df_dict = s3_stream_to_dict(s3_select_query_result)
+
+    # Convert lst of dicts into geopandas dataframe
+    start = timeit.default_timer()
     stream_df = gpd.GeoDataFrame.from_records(df_dict)
     stream_df['index'] = stream_df['properties'].apply(lambda x : x['index'])
+    end = timeit.default_timer()
+    logger.info(f"Convert list of dicts into geopandas dataframe process completed. Time Taken: {end - start}s")     
+    
+    # download validation file
+    obj_downloaded = download_s3_object(s3_client, S3_BUCKET, 'city-hex-polygons-8.geojson')
 
-
+    # Read Validation File
+    start = timeit.default_timer()
+    if obj_downloaded:
+        validate_df = gpd.read_file('./city-hex-polygons-8.geojson', driver="GeoJSON")
+    end = timeit.default_timer()
+    logger.info(f"Read Validation file into geopandas dataframe process completed. Time Taken: {end - start}s")     
+        
+    # Validate Stream Data aginst validation file
+    start = timeit.default_timer()
     validation_df = stream_df['index'].compare(validate_df['index'])
+    end = timeit.default_timer()
+    logger.info(f"Validate Stream Data aginst validation file process completed. Time Taken: {end - start}s")     
 
     return validation_df
 
 if __name__ == '__main__':
+    start = timeit.default_timer()
+
+    # Run the validation process
     validation_df = validation_run()
+
+    # Evaluate result
     if validation_df.empty:
-        print("Validation Succeeded!")
+        status = "Validation Succeeded!"
     else:
-        raise Exception("Validation Failed!")
+        status = "Validation Failed!"
+    
+    # Return process time
+    end = timeit.default_timer()
+    logger.info(f"Challenge 1 Completed. {status} Time Taken: {end - start}s")
--- src/packages/boto_service.py
+++ src/packages/boto_service.py
@@ -3,17 +3,32 @@ import os
 import requests
 import botocore
 import json
+import timeit
+from loguru import logger
 
 # Constants
 SECRETS_URL = "https://cct-ds-code-challenge-input-data.s3.af-south-1.amazonaws.com/ds_code_challenge_creds.json"
 S3_REGION = "af-south-1"
 S3_BUCKET = "cct-ds-code-challenge-input-data"
 
+def timer_log(func):
+    # This function shows the execution time of the inner function
+    def wrap_func(*args, **kwargs):
+        start = timeit.default_timer()
+        result = func(*args, **kwargs)
+        end = timeit.default_timer()
+        # print(f'Function {func.__name__!r} executed in {(end - start):.4f} seconds')
+        logger.info(f"{func.__name__!r} function completed. Time Taken: {end - start}s")
+        return result
+    return wrap_func
+
+@timer_log
 def get_s3_secrets(url: str):
     response = requests.get(SECRETS_URL).json()
     keys = response['s3']
     return keys['access_key'], keys['secret_key']
     
+@timer_log
 def init_s3_client(s3_region : str):
     access_key, secret_key = get_s3_secrets(SECRETS_URL)
     s3_client = boto3.client("s3",
@@ -23,12 +38,14 @@ def init_s3_client(s3_region : str):
                             )
     return s3_client
 
+@timer_log
 def get_s3_object_list(s3_client: botocore.client, s3_bucket: str):
     s3_bucket_objects = s3_client.list_objects(Bucket = s3_bucket)
     files = s3_bucket_objects['Contents']
     objects = [file['Key'] for file in files]
     return objects
 
+@timer_log
 def download_s3_object(s3_client: botocore.client, s3_bucket: str, filename: str):
     objects = get_s3_object_list(s3_client, s3_bucket)
     filename = 'city-hex-polygons-8.geojson'
@@ -46,6 +63,7 @@ def download_s3_object(s3_client: botocore.client, s3_bucket: str, filename: str
         
     return download_complete
 
+@timer_log
 def s3_select_query(s3_client: botocore.client, s3_bucket: str, object: str, resolution: int):
     response = s3_client.select_object_content(
             Bucket = s3_bucket,
@@ -58,6 +76,7 @@ def s3_select_query(s3_client: botocore.client, s3_bucket: str, object: str, res
     
     return response['Payload']
 
+@timer_log
 def s3_stream_to_dict(query_result: botocore.eventstream):
     parsed_response = ""
     for event in query_result:
